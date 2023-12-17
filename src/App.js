148f20a01fc56be3ca0eb32cd0e0f7183831c836
Shreyash D. Dhasade <2021.shreyash.dhasade@ves.ac.in>
2023-12-17 20:50:37 +0530

    1st commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import { Mylogin } from './Mylogin';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import { Player } from './Player';
+import { useDataLayerValue } from './DataLayer';
+
+const spotify = new SpotifyWebApi();
+
+
+function App() {
+  const [token, setToken] = useState(null);
+  const [{ user }, dispatch] = useDataLayerValue();
+
+  // Run code based on given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if(_token){
+      setToken(_token);
+      
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user)=> {
+        // console.log('User from Spotify API:', user);
+
+        // dispatch({
+        //   type: 'SET_USER',
+        //   user: user,
+        // });
+      });
+    } 
+
+    console.log("I have a token -> ", token);
+  }, []);
+
+  // console.log("shrey", user);
+
+  return (
+    <div className="App">
+      {
+        token ? (<Player/>) : (<Mylogin/>)
+      }
+      
+      </div>
+  );
+}
+
+export default App;
